ab1993fe4ac306a881166ebb20d02848538a8d30
David Duefrene <fortismilite@gmail.com>
2022-05-11 15:15:19 -0600

    Initial commit

---
+++ ThemeController.js
@@ -0,0 +1,39 @@
+class ThemeController {
+    constructor(themeLists, startTheme = 'None', updateThemeCallback = () => {}) {
+        this.Themes = themeLists;
+        this.ThemeList = Object.keys(themeLists);
+        this.CurrentTheme = startTheme;
+        this.updateThemeCallback = updateThemeCallback; // TODO get this working
+        Object.keys(this.Themes[startTheme]).forEach(el => {
+            document.documentElement.style.setProperty(`--${el}`, this.Themes[startTheme][el]);
+        });
+    }
+
+    setTheme(theme) {
+        try {
+            Object.keys(this.Themes[theme]).forEach(el => {
+                document.documentElement.style.setProperty(`--${el}`, this.Themes[theme][el]);
+            });
+            this.CurrentTheme = theme;
+            this.updateThemeCallback(theme);
+
+            // create the event
+            let themeChange = new CustomEvent('themeChange', {
+                detail: { theme: theme }
+            });
+            // dispatch the event
+            document.dispatchEvent(themeChange);
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    getTheme() {
+        return this.CurrentTheme;
+    }
+
+    getThemeList() {
+        return this.ThemeList;
+    }
+}

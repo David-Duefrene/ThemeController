a65bcc8629efca037394abd102472b786ecb84fc
David Duefrene <fortismilite@gmail.com>
2022-08-03 15:37:02 -0600

    main.ts has been linted

--- main.ts
+++ main.ts
@@ -1,34 +1,34 @@
-import ThemeController from './ThemeController';
+import ThemeController from './ThemeController'
 import './styles.css'
 
 const themes = {
-    "Light": {
-        "main-color": "hsl(0, 0%, 13%)",
-        "text-color": "black",
-        "line-color": "hsl(267, 95%, 76%)",
-        "alt-color": "hsl(0, 0%, 66%)"
-    },
-    "Dark": {
-        "main-color": "hsl(0, 0%, 90%)",
-        "text-color": "white",
-        "line-color": "hsl(267, 95%, 76%)",
-        "alt-color": "hsl(0, 0%, 66%)"
-    }
+	'Light': {
+		'main-color': 'hsl(0, 0%, 13%)',
+		'text-color': 'black',
+		'line-color': 'hsl(267, 95%, 76%)',
+		'alt-color': 'hsl(0, 0%, 66%)',
+	},
+	'Dark': {
+		'main-color': 'hsl(0, 0%, 90%)',
+		'text-color': 'white',
+		'line-color': 'hsl(267, 95%, 76%)',
+		'alt-color': 'hsl(0, 0%, 66%)',
+	},
 }
-let currentTheme = "Light";
-document.getElementById("currentTheme").innerHTML = currentTheme;
+let currentTheme = 'Light'
+document.getElementById('currentTheme',).innerHTML = currentTheme
 
-document.addEventListener('themeChange', (eve: any) => {
-    currentTheme = eve.detail.theme;
-    console.table(eve);
-    console.log(`Theme: ${currentTheme}`);
-    document.getElementById("currentTheme").innerHTML = currentTheme;
-})
+// For demo purposes
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+document.addEventListener('themeChange', (eve: any,) => {
+	currentTheme = eve.detail.theme
+	document.getElementById('currentTheme',).innerHTML = currentTheme
+},)
 
-let theme_controller = new ThemeController(themes, currentTheme);
+const testThemeController = new ThemeController(themes, currentTheme,)
 
 const switchTheme = () => {
-    theme_controller.theme = currentTheme == "Light" ? "Dark" : "Light";
+	testThemeController.theme = currentTheme === 'Light' ? 'Dark' : 'Light'
 }
 
-document.getElementById("ChangeThemeButton").addEventListener("click", switchTheme);
+document.getElementById('ChangeThemeButton',).addEventListener('click', switchTheme,)
